cea19298f4f34cbc5f4c1338949b99b9df2d622d
Andrey Prokhorenko <_@example.com>
2026-03-25 01:27:14 +0500

    fix: change localhost to render url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // Я добавил /api в конец ссылки, чтобы сервер тебя "услышал"
- baseURL: 'https://dashboard-x.onrender.com',
- 
+  baseURL: 'https://dashboard-x.onrender.com', 
 });
 
 export const getSocialStats = () => API.get('/stats');
 export const updateStat = (data) => API.post('/social-stats', data);
 
 export default API;
+
 
 
